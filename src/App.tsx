5adeb2490d696e899bdcac06bac612784d128672
dgs99001 <dgs99001@proton.me>
2025-12-05 15:21:27 +0800

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ const apps = [
   {
     id: 1,
     name: "非凡娱乐",
-    description: "新台大水",
+    description: "",
     iconUrl: "https://pic1.imgdb.cn/item/68692e6658cb8da5c89176fd.jpg",
     url: "https://wcwx.paradisemall.net/app/register.php?site_id=1050&topId=880722&selfPlanId=855049",
   },
